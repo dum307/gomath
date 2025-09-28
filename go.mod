e8ff03ebc0e4e6bac94090cfbb3283ebcabcdc57
Dmitriy Urazalin <dmitriy.urazalin@vizor-games.com>
2025-09-28 22:47:52 +0300

    gomath v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dum307/gomath
+
+go 1.24.4
---
+++ math.go
@@ -0,0 +1,11 @@
+package gomath
+
+// Add складывает два числа
+func Add(a, b int) int {
+    return a + b
+}
+
+// Multiply умножает два числа  
+func Multiply(a, b int) int {
+    return a * b
+}
