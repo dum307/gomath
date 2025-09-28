82611853589c7d03cd81b0b99a18134cfb5c0a3e
Dmitriy Urazalin <dmitriy.urazalin@vizor-games.com>
2025-09-28 22:57:27 +0300

    Добавлены функции Sub и Divide

--- math.go
+++ math.go
@@ -9,3 +9,16 @@ func Add(a, b int) int {
 func Multiply(a, b int) int {
     return a * b
 }
+
+// Sub вычитает b из a
+func Sub(a, b int) int {
+    return a - b
+}
+
+// Divide делит a на b
+func Divide(a, b int) int {
+    if b == 0 {
+        return 0
+    }
+    return a / b
+}
